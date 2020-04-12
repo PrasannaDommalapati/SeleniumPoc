83e91497fe886e14c8a9cb03a3e562c2d416fa70
Prasanna Kumar <prasanna.kumar@headforwards.com>
2020-04-12 19:43:59 +0100

    More Extensions

--- SeleniumPoc/SeleniumPoc/ElementExtension.cs
+++ SeleniumPoc/SeleniumPoc/ElementExtension.cs
@@ -28,5 +28,77 @@ namespace SeleniumPoc
                 throw new SeleniumPocException(ex.Message);
             }
         }
+
+        public static void Clear(this By identifier, IWebDriver driver)
+        {
+            try
+            {
+                driver.FindWebElement(identifier).Clear();
+            }
+            catch (Exception ex)
+            {
+                throw new SeleniumPocException(ex.Message);
+            }
+        }
+
+        public static void Click(this By identifier, IWebDriver driver)
+        {
+            try
+            {
+                driver.FindWebElement(identifier).Click();
+            }
+            catch (Exception ex)
+            {
+                throw new SeleniumPocException(ex.Message);
+            }
+        }
+
+        public static string GetText(this By identifier, IWebDriver driver)
+        {
+            try
+            {
+                return driver.FindWebElement(identifier).Text;
+            }
+            catch (Exception ex)
+            {
+                throw new SeleniumPocException(ex.Message);
+            }
+        }
+
+        public static bool IsElementEnabled(this By identifier, IWebDriver driver)
+        {
+            try
+            {
+                return driver.FindWebElement(identifier).Enabled;
+            }
+            catch (Exception ex)
+            {
+                throw new SeleniumPocException(ex.Message);
+            }
+        }
+
+        public static bool IsElementDisplayed(this By identifier, IWebDriver driver)
+        {
+            try
+            {
+                return driver.FindWebElement(identifier).Displayed;
+            }
+            catch (Exception ex)
+            {
+                throw new SeleniumPocException(ex.Message);
+            }
+        }
+
+        public static bool Selected(this By identifier, IWebDriver driver)
+        {
+            try
+            {
+                return driver.FindWebElement(identifier).Selected;
+            }
+            catch (Exception ex)
+            {
+                throw new SeleniumPocException(ex.Message);
+            }
+        }
     }
 }
